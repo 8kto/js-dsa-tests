bea0d501f21b2e890ebdeea4054478e8aa5c0de3
Okto <web@axisful.info>
2022-01-25 23:32:33 +0100

    Add BFS tree traversal

---
+++ dataStructures/breadthFirstSearch.js
@@ -0,0 +1,40 @@
+/**
+ * @link https://levelup.gitconnected.com/finding-the-shortest-path-in-javascript-pt-1-breadth-first-search-67ae4653dbec
+ */
+let breadthFirstSearch = (tree, rootNode, searchValue) => {
+  // make a queue array
+  let queue = []
+  let path = []
+  // populate it with the node that will be the root of your search
+  queue.push(rootNode)
+
+  // search the queue until it is empty
+  while (queue.length > 0) {
+    // assign the top of the queue to variable currentNode
+    let currentNode = queue[0]
+    path.push(currentNode.value)
+
+    // if currentNode is the node we're searching for, break & alert
+    if (currentNode.value === searchValue) {
+      return path
+    }
+
+    // if currentNode has a left child node, add it to the queue.
+    if (currentNode.left !== null) {
+      queue.push(tree[currentNode.left])
+    }
+
+    // if currentNode has a right child node, add it to the queue.
+    if (currentNode.right !== null) {
+      queue.push(tree[currentNode.right])
+    }
+
+    // remove the currentNode from the queue.
+    queue.shift()
+  }
+
+  console.log('Sorry, no such node found :(')
+  return null
+}
+
+module.exports = { breadthFirstSearch }
---
+++ dataStructures/breadthFirstSearch.test.js
@@ -0,0 +1,89 @@
+// const { breadthFirstSearch } = require('./breadthFirstSearch')
+
+const breadthFirstSearch = (tree, root, value) => {
+  const queue = []
+  const path = []
+
+  queue.push(root)
+  while (queue.length) {
+    const node = queue.shift()
+    path.push(node.value)
+
+    if (value === node.value) return path
+    if (node.left !== null) queue.push(tree[node.left])
+    if (node.right !== null) queue.push(tree[node.right])
+  }
+
+  return null
+}
+
+/**
+ 10
+ /  \
+ 4    17
+ / \   / \
+ 1   9 12  18
+ */
+const getTree = () => ({
+  '10': {
+    value: '10',
+    left: '4',
+    right: '17',
+  },
+  '4': {
+    value: '4',
+    left: '1',
+    right: '9',
+  },
+  '17': {
+    value: '17',
+    left: '12',
+    right: '18',
+  },
+  '1': {
+    value: '1',
+    left: null,
+    right: null,
+  },
+  '9': {
+    value: '9',
+    left: null,
+    right: null,
+  },
+  '12': {
+    value: '12',
+    left: null,
+    right: null,
+  },
+  '18': {
+    value: '18',
+    left: null,
+    right: null,
+  },
+})
+
+describe('BinarySearchTree', () => {
+  let tree
+
+  beforeEach(() => {
+    tree = getTree()
+  })
+
+  it('searches nodes', () => {
+    expect(breadthFirstSearch(tree, tree[10], '12')).toEqual(
+      ['10', '4', '17', '1', '9', '12'],
+    )
+  })
+
+  it('does not find nodes', () => {
+    expect(breadthFirstSearch(tree, tree[10], '42')).toEqual(
+      null,
+    )
+  })
+
+  it('searches nodes from subtree', () => {
+    expect(breadthFirstSearch(tree, tree[17], '18')).toEqual(
+      ['17', '12', '18'],
+    )
+  })
+})
