3347cc627ca72127cbe6500a4defa11d890e69b9
Igor Ivlev <igor.ivlev@verkstedt.com>
2022-02-09 14:07:41 +0100

    Reverse linked list

---
+++ dataStructures/linkedList.test.ts
@@ -0,0 +1,45 @@
+type LinkedListItem = {
+  next: LinkedListItem | null
+  data: unknown
+}
+
+export const reverseLinkedList = (
+  list: LinkedListItem | null
+): LinkedListItem | null => {
+  let prev = null,
+    cur = list,
+    next = list
+
+  while (cur) {
+    next = next && next.next
+    cur.next = prev
+    prev = cur
+    cur = next
+  }
+
+  return prev
+}
+
+describe('Linked lists', () => {
+  let list: LinkedListItem
+
+  beforeEach(() => {
+    list = {
+      data: 1,
+      next: { data: 2, next: { data: 3, next: null } },
+    }
+  })
+
+  it('reverseLinkedList', () => {
+    expect(reverseLinkedList(list)).toEqual({
+      'data': 3,
+      'next': {
+        'data': 2,
+        'next': {
+          'data': 1,
+          'next': null,
+        },
+      },
+    })
+  })
+})
